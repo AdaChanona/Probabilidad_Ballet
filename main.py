5a71ab1f1a2b036b917665f21c15d5caa5665c7b
AdaChanona <ada.gamboa.chanona2510@gmail.com>
2021-07-02 02:24:21 -0500

    Empírica y parametrización

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ from tkinter import Text
 from tkinter.constants import INSERT
 from fractions import Fraction
 
-#tkinter
-raiz=tk.Tk()
-raiz.title('Bailarinas de Ballet')
-raiz.geometry('1000x700')
-raiz.pack_propagate(False)
-raiz.config(bg='white',width=1000,height=700)
-
 global posiciones_ballet
 global pasos_ballet
 global bailarinas
@@ -25,38 +18,37 @@ pasos_ballet=['Relevé','Retiré']
 bailarinas= [1,2,3,4,5,6,7,8,9,10]
 equipo_ballet=[]
 
-def equipo_bailarinas():
+def permutaciones_bailarinas():
     equipo= permutations(bailarinas,2)
-
     for i in list(equipo):
         equipo_ballet.append(i)
     per='permutaciones= '+ str(len(equipo_ballet))
     return per
 
-def rango_bailarinas():
+def equipo_bailarinas():
     num_equipo=[]
-    for i in range(5):
+    for i in range(len(posiciones_ballet)):
         num_equipo.append(random.choice(equipo_ballet)) 
     return num_equipo
 
 def dataframe_equipos_seleccionados():
-    num_equipo= rango_bailarinas()
+    num_equipo= equipo_bailarinas()
     cont=1
     df = pd.DataFrame(columns=['Numero de equipo','Integrantes', 'bailarina principal', 'bailarina secundaria'],
-                  index=range(5))
-    for i in range(len(num_equipo)):
+                  index=range(len(posiciones_ballet)))
+    for i in range(len(posiciones_ballet)):
         equipo=num_equipo[i]
         df.iloc[i]=(cont,num_equipo[i],equipo[0],equipo[1])
         cont=cont+1
     return df
 
-def practica_ballet():
+def tabla_fases():
     pasos=[]
     for i in range(len(posiciones_ballet)):
         if posiciones_ballet[i]%2==0:
             pasos.append(pasos_ballet[0])
         else:
-            pasos.append(pasos_ballet[1])
+            pasos.append(pasos_ballet[-1])
     df_pos=pd.DataFrame(posiciones_ballet,columns=['FASE 1 POSICIONES'])
     df_pasos=pd.DataFrame(pasos,columns=['FASE 2 PASOS'])
     df_ballet=pd.concat([df_pos,df_pasos],axis=1)
@@ -141,36 +133,92 @@ def probabilidad_subjetiva_tabla2(prob_pos, df_ballet):
     df_subjetiva2=pd.concat([df1,df2,df3],axis=1)
     return df_subjetiva2
 
-df_subjetivo1, array=probabilidad_subjetiva_tabla1(practica_ballet())
+def conf_parametro():
+    op=0
+    while op<3:
+        print('Escoja una opción')
+        print('1. Ingresar nueva posicion    2. Eliminar posicion   3. Salir')
+        op=int(input())
+        if op==1:
+            print('Estas son las posiciones de ballet: ')
+            print(posiciones_ballet)
+            pos_new=int(input('Ingresa posición de ballet: '))
+            posiciones_ballet.append(pos_new)
+            print('Se agrego la posicion ', posiciones_ballet)
+        elif op==2:
+            print('Estas son las posiciones de ballet: ')
+            print(posiciones_ballet)
+            pos_delete=int(input('Ingresa posición de ballet que deseas eliminar: '))
+            posiciones_ballet.remove(pos_delete)
+            print('Se eliminó la posición ',posiciones_ballet)
 
+def empirico(equipo):
+    num_equipo=np.array(equipo['Numero de equipo'])
+    op=0
+    while op==1 or op==2 or op==0:
+        print('----------------------------------------------------')
+        print('Escoja una opcion')
+        print('1. Configurar simulación')
+        print('2. Simular')
+        print('3. salir')
+        op=int(input())
+        if(op==1):
+            conf_parametro()
+        if(op==2):
+            repeticion=int(input('¿Cuantas veces quiere repetir la simulación? '))
+            for i in range(repeticion):
+                print('-----------------------------------------------------')
+                print('Escogiendo equipo...')
+                print('Te toco el equipo: ',random.choice(num_equipo))
+                print('La posición se escogerá aleatoriamente')
+                pos=random.choice(posiciones_ballet)
+                print('La bailarina principal hará un plié en la posicion: ', pos)
+                if(pos%2==0):
+                    print('La bailarina secundaria hará un: ',pasos_ballet[0])
+                else:
+                    print('La bailarina secundaria hará un: ',pasos_ballet[-1]) 
+
+permutaciones=permutaciones_bailarinas()
+equipo=equipo_bailarinas()
+equipo_select=dataframe_equipos_seleccionados()
+empirico(equipo_select)
+df_subjetivo1, array=probabilidad_subjetiva_tabla1(tabla_fases())
+
+#tkinter
+raiz=tk.Tk()
+raiz.title('Bailarinas de Ballet')
+raiz.geometry('1000x680')
+raiz.pack_propagate(False)
+raiz.config(bg='white',width=900,height=680)
 def run():
-    df_salida,frecuencia=probabilidad_clasica(practica_ballet())
+    df_salida,frecuencia=probabilidad_clasica(tabla_fases())
     label1=tk.Label(raiz,text='Proyecto probabilidad: Bailarinas de ballet',font=(200))
     label1.place(x=300,y=40)
-    label2=tk.Label(raiz,text=equipo_bailarinas(),font=(200))
+    label2=tk.Label(raiz,text=permutaciones,font=(200))
     label2.place(x=650,y=180)
     label3=tk.Label(raiz,text='Equipos seleccionados',font=(50))
     label3.place(x=50,y=100)
     table1 = Text(raiz)
-    table1.insert(INSERT,dataframe_equipos_seleccionados().to_string())
-    table1.place(x=20, y=140,height=100, width=600)
+    table1.insert(INSERT,equipo_select.to_string())
+    table1.place(x=20, y=140,height=150, width=600)
     table2 = Text(raiz)
     label4=tk.Label(raiz,text='Probabilidad Clásica',font=(50))
     label4.place(x=50,y=270)
-    table2.insert(INSERT,practica_ballet().to_string())
-    table2.place(x=20, y=310,height=100, width=300)
+    table2.insert(INSERT,tabla_fases().to_string())
+    table2.place(x=20, y=310,height=150, width=300)
     table3 = Text(raiz)
     table3.insert(INSERT,df_salida.to_string())
     table3.place(x=350, y=310,height=90, width=300)
-    tk.Button(raiz, text="Grafica de barras", command=lambda:mostrar_grafica(df_salida,frecuencia)).place(x=750,y=350)
+    tk.Button(raiz, text="Grafica de barras", command=lambda:mostrar_grafica(df_salida,frecuencia)).place(x=700,y=350)
     label5=tk.Label(raiz,text='Probabilidad Subjetiva',font=(50))
     label5.place(x=50,y=450)
     table4 = Text(raiz)
     table4.insert(INSERT,df_subjetivo1.to_string())
-    table4.place(x=20, y=500,height=100, width=450)
+    table4.place(x=20, y=500,height=150, width=450)
     table5 = Text(raiz)
-    table5.insert(INSERT,probabilidad_subjetiva_tabla2(array, practica_ballet()).to_string())
-    table5.place(x=500, y=500,height=120, width=500)
+    table5.insert(INSERT,probabilidad_subjetiva_tabla2(array, tabla_fases()).to_string())
+    table5.place(x=500, y=500,height=150, width=500)
 
+print('Imprimiendo probabilidades...')
 run()
 raiz.mainloop()
